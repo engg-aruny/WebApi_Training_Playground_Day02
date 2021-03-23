5f4d8bafd07ad9ba26cf9a63097bd910450c1072
Yadav, Arun <Arun.Yadav@crowe.com>
2021-03-23 17:24:27 +0530

    add get employee

---
+++ WebApi_Training_Playground_Day02/WebApi_Training_Playground_Day02/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Web.Http;
+using WebApi_Training_Playground_Day02.Models;
+using WebApi_Training_Playground_Day02.Repositories;
+
+namespace WebApi_Training_Playground_Day02.Controllers
+{
+	public class EmployeeController : ApiController
+	{
+		private readonly IEmployeeRepository _employeeRepository;
+
+		public EmployeeController()
+		{
+			this._employeeRepository = new EmployeeRepository(new WebApiTrainingDbContext());
+		}
+
+		// GET: Employee
+		public IEnumerable<Employee> Get()
+		{
+			return this._employeeRepository.GetEmployees();
+		}
+	}
+}
---
+++ WebApi_Training_Playground_Day02/WebApi_Training_Playground_Day02/Repositories/EmployeeRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi_Training_Playground_Day02.Models;
+
+namespace WebApi_Training_Playground_Day02.Repositories
+{
+	public class EmployeeRepository : IEmployeeRepository
+	{
+		private readonly WebApiTrainingDbContext _context;
+
+		public EmployeeRepository(WebApiTrainingDbContext context)
+		{
+			this._context = context;
+		}
+
+		public IEnumerable<Employee> GetEmployees()
+		{
+			return this._context.Employees.ToList();
+		}
+
+		public Employee GetEmployee(int employeeId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Employee AddEmployee(Employee employee)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Employee UpdateEmployee(Employee employee)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void DeleteEmployee(int employeeId)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
